bb4ee039bf90560931bdc31e0f85c461a5903c62
zenkai <gonzalezwithaz91@gmail.com>
2026-04-15 15:45:45 -0500

    enable cors

--- zenkai-backend/server.js
+++ zenkai-backend/server.js
@@ -5,6 +5,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(cors());
 app.use(express.json());
